665f011fd69684fb1087d8a3a9f1a06cfc522848
eren-celik <elik.dark91@gmail.com>
2022-02-20 17:23:19 +0300

    first commit

---
+++ guTests/guTests.swift
@@ -0,0 +1,16 @@
+//
+//  guTests.swift
+//  guTests
+//
+//  Created by Eren  Çelik on 20.02.2022.
+//
+
+import XCTest
+@testable import gu
+
+class guTests: XCTestCase {
+    
+    func test() {
+        XCTAssertEqual("true", "true")
+    }
+}
