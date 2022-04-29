1939b977d0acdb10424f5a37ec0da5d4833e4302
Eren Celik <63004133+eren-celik@users.noreply.github.com>
2022-04-29 10:34:19 +0300

    Delete Pods directory

--- Pods/Target Support Files/Pods-gu/Pods-gu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_gu : NSObject
-@end
-@implementation PodsDummy_Pods_gu
-@end
--- Pods/Target Support Files/Pods-guTests/Pods-guTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_guTests : NSObject
-@end
-@implementation PodsDummy_Pods_guTests
-@end
